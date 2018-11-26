cef6e6bfc02692648ef52492348e5b0d1ff7160d
Ryan Lopopolo <rjl@hyperbo.la>
2018-11-26 08:27:04 -0800

    Play each track for one minute

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate rodio;
 extern crate walkdir;
 
+use rodio::Source;
 use std::ffi::OsStr;
 use std::io::BufReader;
 use std::path::Path;
+use std::time::Duration;
 use walkdir::{DirEntry, WalkDir};
 
 fn is_music(entry: &DirEntry) -> bool {
@@ -30,7 +32,9 @@ fn main() {
     for entry in walker {
         println!("{}", entry.path().display());
         let file = std::fs::File::open(entry.path()).unwrap();
-        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+        let source = source.take_duration(Duration::from_secs(60));
+        sink.append(source);
 
         sink.sleep_until_end();
     }
