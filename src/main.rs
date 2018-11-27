ba7f34ba3e839a5e0e369d629cee4faeb79a9e1b
Ryan Lopopolo <rjl@hyperbo.la>
2018-11-26 21:49:48 -0800

    Add playlist struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 extern crate rodio;
-extern crate walkdir;
 
-use rodio::Source;
-use std::ffi::OsStr;
-use std::io::BufReader;
+mod playlist;
+
 use std::path::Path;
 use std::time::Duration;
-use walkdir::{DirEntry, WalkDir};
-
-fn is_music(entry: &DirEntry) -> bool {
-    let extension = entry
-        .path()
-        .file_name()
-        .and_then(|p| Path::new(p).extension())
-        .and_then(OsStr::to_str);
-    match extension {
-        Some("mp3") => true,
-        _ => false,
-    }
-}
 
 fn main() {
     let device = rodio::default_output_device().unwrap();
     let sink = rodio::Sink::new(&device);
 
-    let walker = WalkDir::new("/Users/lopopolo/Downloads/test")
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file())
-        .filter(|e| is_music(e));
-    for entry in walker {
-        println!("{}", entry.path().display());
-        let file = std::fs::File::open(entry.path()).unwrap();
-        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-        let source = source.take_duration(Duration::from_secs(60));
-        sink.append(source);
-
+    let config = playlist::Config::new(Duration::new(5, 0), 10);
+    let playlist =
+        playlist::Playlist::from_directory(Path::new("/Users/lopopolo/Downloads/test"), config);
+    for track in playlist {
+        sink.append(track);
         sink.sleep_until_end();
     }
 }
---
+++ src/playlist.rs
@@ -0,0 +1,87 @@
+extern crate rand;
+extern crate walkdir;
+
+use self::rand::Rng;
+use self::walkdir::{DirEntry, WalkDir};
+use rodio::{decoder, source, Source};
+use std::collections::VecDeque;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::{Path, PathBuf};
+use std::time::Duration;
+use std::vec::Vec;
+
+fn is_music(entry: &DirEntry) -> bool {
+    let extension = entry
+        .path()
+        .file_name()
+        .and_then(|p| Path::new(p).extension())
+        .and_then(OsStr::to_str);
+    match extension {
+        Some(ext) if ext == "mp3" => true,
+        _ => false,
+    }
+}
+
+pub struct Config {
+    duration: Duration,
+    count: u64,
+}
+
+impl Config {
+    pub fn new(duration: Duration, count: u64) -> Self {
+        Config { duration, count }
+    }
+}
+
+pub struct Playlist {
+    tracks: VecDeque<PathBuf>,
+    config: Config,
+    cursor: u64,
+}
+
+impl Playlist {
+    pub fn from_directory(dir: &Path, config: Config) -> Self {
+        let mut vec = Vec::new();
+        let walker = WalkDir::new(dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+            .filter(|e| is_music(e));
+        for entry in walker {
+            vec.push(PathBuf::from(entry.path()));
+        }
+        rand::thread_rng().shuffle(&mut vec);
+
+        Playlist {
+            tracks: VecDeque::from(vec),
+            config,
+            cursor: 0,
+        }
+    }
+}
+
+impl Iterator for Playlist {
+    type Item = source::TakeDuration<source::Buffered<decoder::Decoder<BufReader<File>>>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cursor >= self.config.count {
+            return None;
+        }
+        if let Some(p) = self.tracks.pop_front() {
+            File::open(p.as_os_str())
+                .ok()
+                .and_then(|f| rodio::Decoder::new(BufReader::new(f)).ok())
+                .map(|s| s.buffered())
+                .map(|s| s.take_duration(self.config.duration))
+                .map(|s| {
+                    self.cursor += 1;
+                    self.tracks.push_back(p);
+                    s
+                })
+        } else {
+            None
+        }
+    }
+}
