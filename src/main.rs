ad9e5df48ca05caf3be32d9179c888c0f4d51a36
Ryan Lopopolo <rjl@hyperbo.la>
2018-11-27 23:16:48 -0800

    Add a background thread that produces chromecast configuration required for casting

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,71 @@
 #![feature(inner_deref)]
 
+extern crate mdns;
 extern crate mp3_duration;
+#[macro_use]
+extern crate nom;
 extern crate rand;
 extern crate rodio;
 extern crate taglib;
 extern crate walkdir;
 
+mod parser;
 mod playlist;
 
+use mdns::RecordKind;
+use std::collections::HashMap;
+use std::net::IpAddr;
+use std::ops::Deref;
 use std::path::Path;
+use std::sync::{RwLock, Arc};
+use std::thread;
 use std::time::Duration;
 
+const SERVICE_NAME: &'static str = "_googlecast._tcp.local";
+
+struct ChromecastConfig {
+    addr: Option<IpAddr>,
+    txt: HashMap<String, String>,
+}
+
+impl ChromecastConfig {
+    pub fn name(&self) -> Option<&str> {
+        self.txt.get("fn").map(|n| n.deref())
+    }
+}
+
+fn spawn_mdns(registry: Arc<RwLock<HashMap<String, ChromecastConfig>>>) {
+    thread::spawn(move || {
+        for response in mdns::discover::all(SERVICE_NAME).unwrap() {
+            let response = response.unwrap();
+
+            let mut config = ChromecastConfig { addr: None, txt: HashMap::new() };
+            for record in response.records() {
+                match record.kind {
+                    RecordKind::A(addr) => config.addr = Some(addr.into()),
+                    RecordKind::AAAA(addr) => config.addr = Some(addr.into()),
+                    RecordKind::TXT(ref text) => {
+                        let refs = text.iter().map(|s| s.deref()).collect();
+                        config.txt = parser::dns_txt(refs);
+                    },
+                    _ => (),
+                }
+            }
+            let name = config.name().map(String::from);
+            if let Some(name) = name {
+                match registry.write() {
+                    Ok(mut map) => map.insert(name, config),
+                    _ => None,
+                };
+            }
+        }
+    });
+}
+
 fn main() {
+    let registry = Arc::new(RwLock::new(HashMap::new()));
+    spawn_mdns(Arc::clone(&registry));
+
     let device = rodio::default_output_device().unwrap();
     let sink = rodio::Sink::new(&device);
 
@@ -33,6 +87,10 @@ fn main() {
             },
             _ => (),
         }
+        match registry.read() {
+            Ok(map) => println!("{:?}", map.keys()),
+            _ => (),
+        };
         sink.append(track.stream());
         sink.sleep_until_end();
     }
---
+++ src/parser.rs
@@ -0,0 +1,35 @@
+extern crate nom;
+
+use nom::alphanumeric;
+use nom::types::CompleteByteSlice;
+
+use std::str;
+use std::collections::HashMap;
+
+fn complete_byte_slice_to_str<'a>(s: CompleteByteSlice<'a>) -> Result<&'a str, str::Utf8Error> {
+  str::from_utf8(s.0)
+}
+
+named!(key_value<CompleteByteSlice, (&str, &str)>,
+  do_parse!(
+      key: map_res!(alphanumeric, complete_byte_slice_to_str)
+  >>       char!('=')
+  >>  val: map_res!(
+           take_while!(call!(|_| true)),
+           complete_byte_slice_to_str
+         )
+  >>     (key, val)
+  )
+);
+
+// TXT records are given as a Vec of key=value pairs
+pub fn dns_txt(vec: Vec<&str>) -> HashMap<String, String> {
+    let mut collect: HashMap<String, String> = HashMap::new();
+    for txt in vec.iter() {
+        match key_value(CompleteByteSlice(txt.as_bytes())) {
+            Ok((_, (key, value))) => collect.insert(key.to_owned(), value.to_owned()),
+            _ => None,
+        };
+    }
+    collect
+}
