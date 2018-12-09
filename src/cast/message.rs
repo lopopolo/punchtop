e56d182adb5525cbd876a2bbcd004ae91cfd49bf
Ryan Lopopolo <rjl@hyperbo.la>
2018-12-08 20:42:16 -0800

    use imports where possible

--- src/cast/message.rs
+++ src/cast/message.rs
@@ -1,8 +1,9 @@
 use protobuf::{CodedOutputStream, ProtobufResult};
+use serde_json::Error;
 
 use super::payload::*;
-use super::proto;
-use super::Media;
+use super::proto::{CastMessage, CastMessage_PayloadType, CastMessage_ProtocolVersion};
+use super::provider::*;
 
 const DEFAULT_SENDER_ID: &str = "sender-0";
 const DEFAULT_DESTINATION_ID: &str = "receiver-0";
@@ -13,17 +14,17 @@ pub fn encode(msg: impl protobuf::Message, buf: &mut Vec<u8>) -> ProtobufResult<
     output.flush()
 }
 
-pub fn connect() -> Result<proto::CastMessage, serde_json::Error> {
+pub fn connect() -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&connection::Payload::Connect)?;
     Ok(message(connection::NAMESPACE, payload))
 }
 
-pub fn close() -> Result<proto::CastMessage, serde_json::Error> {
+pub fn close() -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&connection::Payload::Close)?;
     Ok(message(connection::NAMESPACE, payload))
 }
 
-pub fn load(request_id: i32, session_id: &str, media: Media) -> Result<proto::CastMessage, serde_json::Error> {
+pub fn load(request_id: i32, session_id: &str, media: Media) -> Result<CastMessage, Error> {
     let media = {
         let config = media::Media {
             content_id: media.url.to_string(),
@@ -45,7 +46,7 @@ pub fn load(request_id: i32, session_id: &str, media: Media) -> Result<proto::Ca
     Ok(message(media::NAMESPACE, payload))
 }
 
-pub fn play(request_id: i32, media_session_id: i32) -> Result<proto::CastMessage, serde_json::Error> {
+pub fn play(request_id: i32, media_session_id: i32) -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&media::Payload::Play {
         request_id,
         media_session_id: media_session_id,
@@ -54,18 +55,18 @@ pub fn play(request_id: i32, media_session_id: i32) -> Result<proto::CastMessage
     Ok(message(media::NAMESPACE, payload))
 }
 
-pub fn ping() -> Result<proto::CastMessage, serde_json::Error> {
+pub fn ping() -> Result<CastMessage, Error> {
     let namespace = "urn:x-cast:com.google.cast.tp.heartbeat";
     let payload = serde_json::to_string(&heartbeat::Payload::Ping)?;
     Ok(message(heartbeat::NAMESPACE, payload))
 }
 
-pub fn pong() -> Result<proto::CastMessage, serde_json::Error> {
+pub fn pong() -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&heartbeat::Payload::Pong)?;
     Ok(message(heartbeat::NAMESPACE, payload))
 }
 
-pub fn launch(request_id: i32, app_id: &str) -> Result<proto::CastMessage, serde_json::Error> {
+pub fn launch(request_id: i32, app_id: &str) -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&receiver::Payload::Launch {
         request_id,
         app_id: app_id.to_owned(),
@@ -76,7 +77,7 @@ pub fn launch(request_id: i32, app_id: &str) -> Result<proto::CastMessage, serde
 pub fn stop(
     request_id: i32,
     session_id: &str,
-    ) -> Result<proto::CastMessage, serde_json::Error> {
+    ) -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&receiver::Payload::Stop {
         request_id,
         session_id: session_id.to_owned(),
@@ -84,15 +85,15 @@ pub fn stop(
     Ok(message(receiver::NAMESPACE, payload))
 }
 
-pub fn receiver_status(request_id: i32) -> Result<proto::CastMessage, serde_json::Error> {
+pub fn receiver_status(request_id: i32) -> Result<CastMessage, Error> {
     let payload = serde_json::to_string(&receiver::Payload::GetStatus { request_id })?;
     Ok(message(receiver::NAMESPACE, payload))
 }
 
-fn message(namespace: &str, payload: String) -> proto::CastMessage {
-    let mut msg = proto::CastMessage::new();
-    msg.set_payload_type(proto::CastMessage_PayloadType::STRING);
-    msg.set_protocol_version(proto::CastMessage_ProtocolVersion::CASTV2_1_0);
+fn message(namespace: &str, payload: String) -> CastMessage {
+    let mut msg = CastMessage::new();
+    msg.set_payload_type(CastMessage_PayloadType::STRING);
+    msg.set_protocol_version(CastMessage_ProtocolVersion::CASTV2_1_0);
     msg.set_namespace(namespace.to_owned());
     msg.set_source_id(DEFAULT_SENDER_ID.to_owned());
     msg.set_destination_id(DEFAULT_DESTINATION_ID.to_owned());
