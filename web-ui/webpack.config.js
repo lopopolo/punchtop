379c9b5b6d1afdcdcbda66797d5b2c8838c8d5d9
Ryan Lopopolo <rjl@hyperbo.la>
2018-12-16 19:47:46 -0800

    Optimize webpack build

--- web-ui/webpack.config.js
+++ web-ui/webpack.config.js
@@ -1,11 +1,25 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production'
 
 const plugins = [
+  new MiniCssExtractPlugin({
+    filename: "[name].css",
+    chunkFilename: "[id].css"
+  }),
   new HtmlWebPackPlugin({
     template: "./src/index.html",
     filename: "./index.html",
-    inlineSource: ".(js|css)$"
+    inlineSource: /\.(js|css)$/,
+    minify: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeComments: true,
+      useShortDoctype: true,
+    }
   }),
   new HtmlWebpackInlineSourcePlugin()
 ];
@@ -25,7 +39,7 @@ module.exports = {
         exclude: /index\.css/,
         use: [
           {
-            loader: "style-loader"
+            loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
