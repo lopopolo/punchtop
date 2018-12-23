c80c9a0fc7f5f776ca08d1ac375ca473d6431146
Ryan Lopopolo <rjl@hyperbo.la>
2018-12-23 13:06:10 -0500

    DRY up cast-client/build.rs

--- cast-client/build.rs
+++ cast-client/build.rs
@@ -4,6 +4,14 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 
+const MOD: &[u8] = b"
+pub use self::authority_keys::*;
+pub use self::cast_channel::*;
+
+mod authority_keys;
+mod cast_channel;
+";
+
 #[derive(Debug)]
 pub struct StrReplace {
     data: String,
@@ -12,11 +20,11 @@ pub struct StrReplace {
 impl StrReplace {
     pub fn from_file(path: &str) -> StrReplace {
         let filepath = Path::new(path);
-        let mut file = File::open(filepath).unwrap();
+        let mut file = File::open(filepath).expect(&format!("Failed to open {}", path));
         let mut data = String::new();
 
         file.read_to_string(&mut data)
-            .expect("Failed to read file.");
+            .expect(&format!("Failed to read {}", path));
 
         StrReplace { data }
     }
@@ -27,16 +35,19 @@ impl StrReplace {
     }
 
     pub fn to_file(&self, dst: &str) {
-        let mut file = File::create(dst).unwrap();
+        let mut file = File::create(dst).expect(&format!("Failed to create {}", dst));
         file.write_all(self.data.as_bytes())
-            .expect("Failed to write file.");
+            .expect(&format!("Failed to write {}", dst));
     }
 }
 
 fn main() {
+    let protos = &["proto/authority_keys.proto", "proto/cast_channel.proto"];
+    let gen = &["src/proto/authority_keys.rs", "src/proto/cast_channel.rs"];
+
     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
         out_dir: "src/proto",
-        input: &["proto/authority_keys.proto", "proto/cast_channel.proto"],
+        input: protos,
         includes: &["proto"],
         customize: protobuf_codegen_pure::Customize {
             ..Default::default()
@@ -44,30 +55,17 @@ fn main() {
     })
     .expect("protoc");
 
+    for proto in gen {
+        // Code mod to silence clippy warnings on nightly about deprecated lints
+        StrReplace::from_file(proto)
+            .replace(
+                "#![allow(clippy)]",
+                "#![allow(clippy::all, clippy::pedantic)]",
+            )
+            .to_file(proto);
+    }
+
     let dest_path = Path::new(".").join("src/proto/mod.rs");
-    let mut f = File::create(&dest_path).unwrap();
-
-    f.write_all(
-        b"
-        pub use self::authority_keys::*;
-        pub use self::cast_channel::*;
-
-        mod authority_keys;
-        mod cast_channel;
-    ",
-    )
-    .unwrap();
-
-    StrReplace::from_file("src/proto/authority_keys.rs")
-        .replace(
-            "#![allow(clippy)]",
-            "#![allow(clippy::all, clippy::pedantic)]",
-        )
-        .to_file("src/proto/authority_keys.rs");
-    StrReplace::from_file("src/proto/cast_channel.rs")
-        .replace(
-            "#![allow(clippy)]",
-            "#![allow(clippy::all, clippy::pedantic)]",
-        )
-        .to_file("src/proto/cast_channel.rs");
+    let mut f = File::create(&dest_path).expect("Failed to create proto/mod.rs");
+    f.write_all(MOD).expect("Failed to write proto/mod.rs");
 }
