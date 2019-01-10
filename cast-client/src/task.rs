aa95bf1c51ef8c61a93196e3715567e6ecf47711
Ryan Lopopolo <rjl@hyperbo.la>
2019-01-10 10:34:57 -0800

    Don't take a double reference

--- cast-client/src/task.rs
+++ cast-client/src/task.rs
@@ -62,7 +62,7 @@ pub fn respond(
     command: &UnboundedSender<Command>,
     status: &UnboundedSender<Status>,
 ) -> impl Future<Item = (), Error = ()> {
-    let responder = Responder::new(&connect, &command, &status);
+    let responder = Responder::new(connect, command, status);
     source
         .for_each(move |message| {
             if let Err(err) = responder.handle(&message) {
